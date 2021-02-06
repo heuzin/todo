529884e9560ebcaf4630d27c90d0651d91bf6000
heuzin <matheusqs.arq@gmail.com>
2021-02-05 21:15:16 -0500

    todos array

---
+++ todo-app/todos-app.js
@@ -0,0 +1,16 @@
+const todos = [{
+    text: 'Order Cat food',
+    completed: false
+}, {
+    text: 'Clean Kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
