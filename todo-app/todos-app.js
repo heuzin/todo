b63fe517e798fc045f3454d471cc6204d7482eb0
heuzin <matheusqs.arq@gmail.com>
2021-02-05 21:22:15 -0500

    todo elements and showing todos left

--- todo-app/todos-app.js
+++ todo-app/todos-app.js
@@ -14,3 +14,17 @@ const todos = [{
     text: 'Exercise',
     completed: true
 }]
+
+const incompleteTodos = todos.filter((todo) => {
+    return !todo.completed
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach((todo) => {
+    const todoEl = document.createElement('p')
+    todoEl.textContent = todo.text
+    document.querySelector('body').appendChild(todoEl)
+})
