d964212512149a241466bd42e9529582a4c2ab7a
heuzin <matheusqs.arq@gmail.com>
2021-02-06 23:37:43 -0500

    removing a todo by its id

--- todo-app/todos-functions.js
+++ todo-app/todos-functions.js
@@ -15,6 +15,17 @@ const saveTodos = (todos) => {
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
+// Remove a todo function
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => {
+        return todo.id === id
+    })
+
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
+
 // Generate the DOM structure for a todo
 const generateTodoDOM = (todo) => {
     const todoEl = document.createElement('div')
@@ -33,6 +44,11 @@ const generateTodoDOM = (todo) => {
     // Setup the remove button
     removeButton.textContent = 'x'
     todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
 
     return todoEl
 }
