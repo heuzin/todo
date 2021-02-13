5ff7e24f7e7a3411ce47151f140c96b1663689c4
heuzin <matheusqs.arq@gmail.com>
2021-02-13 17:22:20 -0300

    views.js for todos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-import { getTodos, createTodo, removeTodo, toggleTodo } from './todos'
+import { renderTodos } from './views'
+import { setFilters } from './filters'
+import { createTodo } from './todos'
 
-console.log(getTodos())
-removeTodo('c0ef32dc-5fc0-4ba4-8fa2-a4af6cf516ef')
-console.log(getTodos())
+renderTodos()
+
+document.querySelector('#search-todo').addEventListener('input', e => {
+    setFilters({
+        searchText: e.target.value
+    })
+    renderTodos()
+})
+
+document.querySelector('#new-todo').addEventListener('submit', e => {
+    const text = e.target.elements.text.value.trim()
+    e.preventDefault()
+    
+    if (text.length > 0) {
+        createTodo(text)
+        renderTodos()
+        e.target.elements.text.value = ''
+    }
+
+})
+
+document.querySelector('#hide-completed').addEventListener('change', e => {
+    setFilters({
+        hideCompleted: e.target.checked
+    })
+    renderTodos()
+})
